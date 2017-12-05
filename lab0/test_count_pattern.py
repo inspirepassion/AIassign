3234a2780d3b852b34d7e0bcfb2da23b4ef5c101
zhixunhe <351680058@qq.com>
2017-12-05 13:33:30 -0800

    get all indices for reoccurant elements

--- lab0/test_count_pattern.py
+++ lab0/test_count_pattern.py
@@ -1,1 +1,12 @@
-def count_pattern(pattern, lst)
+def count_pattern(pattern, lst):
+    num_of_pattern =0
+    len_of_pattern = len(pattern)
+    len_of_lst = len(lst)
+    index_of_firstElement = [i for i, j in enumerate(lst[:len_of_lst-len_of_pattern+1]) if pattern[0] == j]
+    print index_of_firstElement
+
+
+'''def find_index_ofFirstLetter(pattern, lst)
+'''
+
+print(count_pattern(('a', 'b', 'a'), ('g', 'a', 'b', 'a', 'b', 'a', 'b', 'a')))
