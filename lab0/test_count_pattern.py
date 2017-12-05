f1402dde8e5d5b591b26380f651a8f2f635a7e63
zhixunhe <351680058@qq.com>
2017-12-05 13:45:16 -0800

    finish the algorithm for counting number of patterns

--- lab0/test_count_pattern.py
+++ lab0/test_count_pattern.py
@@ -3,10 +3,16 @@ def count_pattern(pattern, lst):
     len_of_pattern = len(pattern)
     len_of_lst = len(lst)
     index_of_firstElement = [i for i, j in enumerate(lst[:len_of_lst-len_of_pattern+1]) if pattern[0] == j]
-    print index_of_firstElement
+    # print index_of_firstElement
+
+    for i in index_of_firstElement:
+        for j in range(1,len_of_pattern):
+            if pattern[j] != lst[i+1]:  break
+            else: num_of_pattern +=1
+    return num_of_pattern
 
 
 '''def find_index_ofFirstLetter(pattern, lst)
 '''
 
-print(count_pattern(('a', 'b', 'a'), ('g', 'a', 'b', 'a', 'b', 'a', 'b', 'a')))
+print(count_pattern(('a', 'b', 'a', 'b'), ('g', 'a', 'b', 'a', 'b', 'a', 'b', 'a')))
