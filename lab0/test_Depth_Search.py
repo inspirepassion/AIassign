9bbec964d3fc44318d2c0a68f5eb65902ada1fa9
Windows <351680058@qq.com>
2017-12-06 19:59:55 -0800

    fixing bug for Depth_Search (Windows)

--- lab0/test_Depth_Search.py
+++ lab0/test_Depth_Search.py
@@ -1,2 +1,28 @@
+count_depth = 0
 def depth(expr):
-    pass
+    global count_depth
+    def Depth_Recurssion(input):
+        global count_depth
+        for i in input:
+            # print i
+            if isinstance(i, (list, tuple)) == False:
+                if (count_depth == 0):
+                    count_depth = 1
+                    return 1
+            else:
+                if 1+Depth_Recurssion(i) >= count_depth:
+                    count_depth = 1+Depth_Recurssion(i)
+                return 1+Depth_Recurssion(i)
+
+    if isinstance(expr, (list, tuple)) == False:
+       return count_depth
+    else:
+        Depth_Recurssion(expr)
+    return count_depth
+
+# print depth('x')
+# print depth(('expt', 'x', 2))
+print depth((('x')))
+# print depth(('/', ('expt', 'x', 5), ('expt', ('-', ('expt', 'x', 2),1), ('/', 5, 2))))
+
+
