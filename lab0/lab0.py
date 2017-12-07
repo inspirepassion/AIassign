c7067a24d788023ef592c306d350d1df39bc4986
zhixunhe <351680058@qq.com>
2017-12-07 08:54:33 -0800

    update lab0.py for Depth_Search

--- lab0/lab0.py
+++ lab0/lab0.py
@@ -67,6 +67,18 @@ def count_pattern(pattern, lst):
 # Problem 2.2: Expression depth
 
 def depth(expr):
+    initNum = -1
+
+    def Depth_Recurssion(enter):
+        if isinstance(enter, (list, tuple)) == False:
+                return 1
+        else:
+            elem_list = [None]*len(enter)
+            for i,j in enumerate(enter):
+                elem_list[i] = 1+Depth_Recurssion(j)
+            elem_list.sort(reverse=True)
+            return elem_list[0]
+    return Depth_Recurssion(expr) + initNum
     raise NotImplementedError
 
 
