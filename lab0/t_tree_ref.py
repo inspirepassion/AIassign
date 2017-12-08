7c11e9b6d0660e639b8b5baa9197df03c4844615
Windows <351680058@qq.com>
2017-12-08 10:32:22 -0800

    finish tree_ref and update it in lab0

--- lab0/t_tree_ref.py
+++ lab0/t_tree_ref.py
@@ -1,6 +1,10 @@
-from unittest import TestCase
+def tree_ref(tree, index):
+    if isinstance(tree, (tuple, list)) == False:
+        return tree
+    elif len(index)==0:
+        return tree
+    else:
+        return tree_ref(tree[index[0]], index[1:])
 
-
-class TestTree_ref(TestCase):
-    def test_tree_ref(self):
-        self.fail()
+tree = (((1, 2), 3), (4, (5, 6)), 7, (8, 9, 10))
+print tree_ref(tree, (1,1,1))
