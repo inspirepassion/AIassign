c26f66b1afea05629e8a06d05d0f529975179bfc
zhixunhe <351680058@qq.com>
2017-12-09 13:16:22 -0800

    commit before pull

--- lab0/t_tree_ref.py
+++ lab0/t_tree_ref.py
@@ -1,6 +1,10 @@
-from unittest import TestCase
+def t_tree_ref(tree, index):
+    if isinstance(tree, (tuple, list)) == False:
+        return tree
+    elif len(index) == 1:
+        return tree
+    else:
+        return t_tree_ref(tree[index[0]], index.pop(0))
 
-
-class TestTree_ref(TestCase):
-    def test_tree_ref(self):
-        self.fail()
+tree = (((1, 2), 3), (4, (5, 6)), 7, (8, 9, 10))
+print(t_tree_ref(tree, (3,1)))
