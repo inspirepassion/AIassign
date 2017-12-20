e674b556ddad0e7d52303c08860cb9b3feb90dff
zhixunhe <351680058@qq.com>
2017-12-20 01:27:18 -0800

    finish bfs

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -38,9 +38,56 @@ from search import Graph
 # If you implement these, the offline tester will test them.
 # If you don't, it won't.
 # The online tester will not test them.
-
+import Queue
 def bfs(graph, start, goal):
+    if start == goal: return [start]
+    dict_nodes = {}
+    for node in graph.nodes:
+        dict_nodes[node] = [0, None]
 
+    result = []
+    q = Queue.Queue()
+
+    dict_nodes[start] = [1, None]
+    q.put(start)
+    while q.queue.__len__() != 0:
+        u = q.get()
+        for v in graph.get_connected_nodes(u):
+            if dict_nodes[v][0] == 0:
+                dict_nodes[v][0] = 1
+                if v is not start:
+                    dict_nodes[v][1] = u
+                # if v == goal:
+                #     break
+                # else:
+                #     q.put(v)
+                q.put(v)
+        # if dict_nodes[goal][1] != None:
+        #     break
+    if dict_nodes[goal][1] == None:
+        return result
+
+    else:
+        link = dict_nodes[goal][1]
+        result.append(goal)
+        # print "goal is: {}".format(goal)
+        # print "goal's link is: {}\n".format(dict_nodes[goal][1])
+        # print "{}'s link is: {}\n".format(dict_nodes[goal][1], dict_nodes[dict_nodes[goal][1]][1])
+        # print "{}'s link is: {}\n".format(dict_nodes[dict_nodes[goal][1]][1], dict_nodes[dict_nodes[dict_nodes[goal][1]][1]][1])
+        # print "S's link is: {}".format(dict_nodes['S'][1])
+        # print "start is: {}".format(start)
+        # print "start = S: {}".format(start=='S')
+        # print "Stairs' p_link is: {}\n".format(dict_nodes['Stairs'][1])
+        # print "Grand Hall's p_link is: {}".format(dict_nodes['Grand Hall'][1])
+
+        while link is not start:
+            # print "While Loop: link is: {}".format(link)
+            result.insert(0, link)
+            link = dict_nodes[link][1]
+        result.insert(0, start)
+
+        # return type(result)
+        return ''.join(result)
     raise NotImplementedError
 
 ## Once you have completed the breadth-first search,
--- lab2/scratch.py
+++ lab2/scratch.py
@@ -1,12 +1,100 @@
 from search import *
+from graphs import *
+import Queue
 
 def main():
     print type(GRAPH1.nodes)
-    print(GRAPH1.nodes)
+    # print(NEWGRAPH1.nodes)
+    print "result is:\n{}".format(bfs(NEWGRAPH1, 'S', 'H'))
 
+
+    '''NEWGRAPH1, 'S', 'H
+    answer:SCDH'''
+def bfs(graph, start, goal):
+    if start == goal: return [start]
     dict_nodes = {}
-    for node in GRAPH1.nodes:
+    for node in graph.nodes:
         dict_nodes[node] = [0, None]
+
+    result = []
+    q = Queue.Queue()
+
+    dict_nodes[start] = [1,None]
+    q.put(start)
+    while q.queue.__len__() != 0:
+        u = q.get()
+        for v in graph.get_connected_nodes(u):
+            if dict_nodes[v][0] == 0:
+                dict_nodes[v][0]=1
+                if v is not start:
+                    dict_nodes[v][1] = u
+                # if v == goal:
+                #     break
+                # else:
+                #     q.put(v)
+                q.put(v)
+        # if dict_nodes[goal][1] != None:
+        #     break
+    if dict_nodes[goal][1] == None:
+        return result
+
+    else:
+        link = dict_nodes[goal][1]
+        result.append(goal)
+        # print "goal is: {}".format(goal)
+        # print "goal's link is: {}\n".format(dict_nodes[goal][1])
+        # print "{}'s link is: {}\n".format(dict_nodes[goal][1], dict_nodes[dict_nodes[goal][1]][1])
+        # print "{}'s link is: {}\n".format(dict_nodes[dict_nodes[goal][1]][1], dict_nodes[dict_nodes[dict_nodes[goal][1]][1]][1])
+        # print "S's link is: {}".format(dict_nodes['S'][1])
+        # print "start is: {}".format(start)
+        # print "start = S: {}".format(start=='S')
+        # print "Stairs' p_link is: {}\n".format(dict_nodes['Stairs'][1])
+        # print "Grand Hall's p_link is: {}".format(dict_nodes['Grand Hall'][1])
+
+        while link is not start:
+            # print "While Loop: link is: {}".format(link)
+            result.insert(0, link)
+            link = dict_nodes[link][1]
+        result.insert(0, start)
+
+        # return type(result)
+        return ''.join(result)
+
+
+
+
+
+    # print "the 'Common Area's connected nodes are: {} ".format(GRAPH1.get_connected_nodes('Common Area'))
+
+    #
+    # q=Queue.Queue()
+    #
+    # q.put(1)
+    # q.put(2)
+    # q.put(3)
+    # for i in q.queue:
+    #     print i
+    # print "the get returns {}".format(q.get())
+    # for i in q.queue:
+    #     print i
+    # q.get()
+    # q.put(4)
+    # print "\n"
+    # for i in q.queue:
+    #     print i
+    # print (q.queue.__len__())
+    '''
+    for i in range(1,14,2):
+        q.put(i)
+
+    q.put('a')
+    q.get()
+    q.put('b')
+
+    print type(q)
+    print q
+    for i in q.queue:
+        print i'''
 
 
 GRAPH1 = Graph(edgesdict = \
